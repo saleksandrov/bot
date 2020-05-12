c4876df7eb365e958204b0c4220d1bac179e5ff1
Sergey Aleksandrov <saleksandrov@gmail.com>
2020-05-12 18:46:58 +0300

    Improved logging

--- bot-base/src/main/kotlin/ru/asv/bot/adapter/WeatherAdapter.kt
+++ bot-base/src/main/kotlin/ru/asv/bot/adapter/WeatherAdapter.kt
@@ -4,18 +4,12 @@ import org.slf4j.LoggerFactory
 import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Mono
 import java.time.Duration
-import java.util.concurrent.locks.ReentrantLock
 
 class WeatherAdapter {
 
     private val log = LoggerFactory.getLogger(WeatherAdapter::class.java)
 
-    companion object {
-        private val cache = Cache()
-    }
-
     fun getWeather(): Mono<WeatherInfo> {
-
         return WebClient.create("https://api.weather.yandex.ru")
                 .get()
                 .uri(
@@ -40,42 +34,3 @@ data class Fact(
     val feels_like: Float,
     val wind_speed: Float
 )
-
-class Cache {
-
-    private val log = LoggerFactory.getLogger(Cache::class.java)
-
-    private val lock = ReentrantLock()
-    private val cacheTimeMls = 30*60*1000
-
-    private var lastUpdateTime: Long = 0
-    private lateinit var weatherInfo: WeatherInfo
-
-    fun getOrSet(calculate: () -> Mono<WeatherInfo>): Mono<WeatherInfo> {
-        TODO("consider to remove")
-        /*
-        val currentTime = LocalDateTime.now().atZone(ZoneId.of("Europe/Moscow")).toInstant().toEpochMilli()
-        if ((currentTime - lastUpdateTime) < cacheTimeMls) {
-            log.info("Get Weather from cache")
-            return Mono.just(weatherInfo)
-        }
-        try {
-            lock.lock()
-            log.info("Request Weather from server")
-            weatherInfo = calculate()
-            lastUpdateTime = currentTime
-            return weatherInfo
-        } finally {
-            lock.unlock()
-        }*/
-    }
-
-}
-
-fun main() {
-
-    println(WeatherAdapter().getWeather())
-    println(WeatherAdapter().getWeather())
-    println(WeatherAdapter().getWeather())
-
-}
