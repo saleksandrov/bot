9dfa8e89b787a7a5303edfaac9099965240e90cb
Sergey Aleksandrov <saleksandrov@gmail.com>
2020-08-18 16:51:40 +0300

    Functional extended

--- bot-base/src/main/kotlin/ru/asv/bot/BotApplication.kt
+++ bot-base/src/main/kotlin/ru/asv/bot/BotApplication.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.WebApplicationType
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.PropertySource
-import ru.asv.bmd.base.config.YamlPropertySourceFactory
+import ru.asv.bot.config.YamlPropertySourceFactory
 
 @PropertySource(value = ["classpath:bot-base-config.yaml", "file:config/bot-base-config.yaml"],
     factory = YamlPropertySourceFactory::class,
